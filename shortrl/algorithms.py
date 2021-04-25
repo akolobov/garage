b0f70d4487381cad2f11aa17ab2fdbb7e3482db6
chinganc <chinganc@microsoft.com>
2021-04-25 10:22:46 -0700

    add refactored codes for running different algorithms

---
+++ shortrl/algorithms.py
@@ -0,0 +1,220 @@
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+from garage.envs import GymEnv
+from garage.sampler import RaySampler
+from garage.torch import prefer_gpu
+from garage.replay_buffer import PathBuffer
+from garage.sampler import FragmentWorker, LocalSampler
+from garage.torch.policies import GaussianMLPPolicy, TanhGaussianMLPPolicy, DeterministicMLPPolicy
+from garage.torch.q_functions import ContinuousMLPQFunction
+from garage.torch.value_functions import GaussianMLPValueFunction
+from garage.torch.optimizers import OptimizerWrapper, ConjugateGradientOptimizer
+
+def get_algo(env, algo_name, discount,
+             use_gpu=False,
+             n_epochs=None,  # number of training epochs
+             batch_size=None,  # batch size of the env sampler
+             policy_network_hidden_sizes=(64, 64),
+             policy_network_hidden_nonlinearity=torch.tanh,
+             value_natwork_hidden_sizes=(64, 64),
+             value_network_hidden_nonlinearity=torch.tanh,
+             optimization_lr=2.5e-4,  # optimization stepsize
+             optimization_minibatch_size=128,  # optimization/replaybuffer minibatch size
+             optimization_n_grad_steps=1000,  # number of gradient updates
+             kl_constraint=0.05,  # kl constraint between policy updates
+             gae_lambda=0.98,  # lambda of gae estimator
+             steps_per_epoch=1,  # number of internal epochs steps per epoch
+             n_workers=4,  # number of workers for data collection
+             **kwargs):
+    # return alg for env with discount
+
+    assert isinstance(env, GymEnv)
+    assert n_epochs is not None
+    assert batch_size is not None
+
+    def get_mlp_policy(stochastic, use_tanh):
+        if stochastic and use_tanh:
+            return TanhGaussianMLPPolicy(
+                        env_spec=env.spec,
+                        hidden_sizes=policy_network_hidden_sizes,
+                        hidden_nonlinearity=policy_network_hidden_nonlinearity,
+                        output_nonlinearity=None,
+                        min_std=np.exp(-20.),
+                        max_std=np.exp(2.))
+
+        if stochastic and not use_tanh:
+            return GaussianMLPPolicy(env.spec,
+                        hidden_sizes=policy_network_hidden_sizes,
+                        hidden_nonlinearity=policy_network_hidden_nonlinearity,
+                        output_nonlinearity=None)
+
+        if not stochastic:
+            return DeterministicMLPPolicy(
+                        env_spec=env.spec,
+                        hidden_sizes=policy_network_hidden_sizes,
+                        hidden_nonlinearity=policy_network_hidden_nonlinearity,
+                        output_nonlinearity=torch.tanh if use_tanh else None)
+
+    def get_mlp_value(form='Q'):
+        if form=='Q':
+            return ContinuousMLPQFunction(
+                    env_spec=env.spec,
+                    hidden_sizes=value_natwork_hidden_sizes,
+                    hidden_nonlinearity=value_network_hidden_nonlinearity,
+                    output_nonlinearity=None)
+        if form=='V':
+            return GaussianMLPValueFunction(
+                    env_spec=env.spec,
+                    hidden_sizes=policy_network_hidden_sizes,
+                    hidden_nonlinearity=value_network_hidden_nonlinearity,
+                    output_nonlinearity=None)
+
+    def get_sampler(policy):
+        return RaySampler(agents=policy,
+                          envs=env,
+                          max_episode_length=env.spec.max_episode_length,
+                          n_workers=n_workers)
+
+    def get_replay_buferr():
+        return PathBuffer(capacity_in_transitions=int(1e6))
+
+    def get_wrapped_optimizer(obj, name='ADAM'):
+        max_optimization_epochs = max(1, int(optimization_n_grad_steps * optimization_minibatch_size / batch_size))
+        return OptimizerWrapper(
+            (torch.optim.Adam, dict(lr=optimization_lr)),
+            obj,
+            max_optimization_epochs=max_optimization_epochs,
+            minibatch_size=optimization_minibatch_size)
+
+    if algo_name=='PPO':
+        from garage.torch.algos import PPO
+        policy = get_mlp_policy(stochastic=True, use_tanh=False)
+        value_function = get_mlp_value('V')
+        sampler = get_sampler(policy)
+        algo = PPO(env_spec=env.spec,
+                   policy=policy,
+                   value_function=value_function,
+                   sampler=sampler,
+                   discount=discount,
+                   center_adv=True,
+                   positive_adv=False,
+                   gae_lambda=gae_lambda,
+                   lr_clip_range=2e-1,  # The limit on the likelihood ratio between policies.
+                   policy_optimizer=get_wrapped_optimizer(policy),
+                   vf_optimizer=get_wrapped_optimizer(value_function),
+                   num_train_per_epoch=steps_per_epoch,
+                )
+
+    elif algo_name=='TRPO':
+        from garage.torch.algos import TRPO
+        policy = get_mlp_policy(stochastic=True, use_tanh=False)
+        value_function = get_mlp_value('V')
+        sampler = get_sampler(policy)
+        policy_optimizer = OptimizerWrapper(
+                            (ConjugateGradientOptimizer, dict(max_constraint_value=kl_constraint)),
+                            policy)
+        algo = TRPO(env_spec=env.spec,
+                    policy=policy,
+                    value_function=value_function,
+                    sampler=sampler,
+                    discount=discount,
+                    center_adv=True,
+                    positive_adv=False,
+                    gae_lambda=gae_lambda,
+                    policy_optimizer=policy_optimizer,
+                    vf_optimizer=get_wrapped_optimizer(value_function),
+                    num_train_per_epoch=steps_per_epoch,
+                    )
+
+    elif algo_name=='VPG':
+        from garage.torch.algos import VPG
+        policy = get_mlp_policy(stochastic=True, use_tanh=False)
+        value_function = get_mlp_value('V')
+        sampler = get_sampler(policy)
+        algo = VPG(env_spec=env.spec,
+                    policy=policy,
+                    value_function=value_function,
+                    sampler=sampler,
+                    discount=discount,
+                    center_adv=True,
+                    positive_adv=False,
+                    gae_lambda=gae_lambda,
+                    policy_optimizer=OptimizerWrapper((torch.optim.Adam, dict(lr=optimization_lr)),policy),
+                    vf_optimizer=get_wrapped_optimizer(value_function),
+                    num_train_per_epoch=steps_per_epoch)
+
+    elif algo_name=='SAC':
+        from garage.torch.algos import SAC
+        policy = get_mlp_policy(stochastic=True, use_tanh=True)
+        qf1 = get_mlp_value('Q')
+        qf2 = get_mlp_value('Q')
+        replay_buffer = get_replay_buferr()
+        sampler = get_sampler(policy)
+        algo = SAC(env_spec=env.spec,
+                   policy=policy,
+                   qf1=qf1,
+                   qf2=qf2,
+                   sampler=sampler,
+                   gradient_steps_per_itr=optimization_n_grad_steps,
+                   max_episode_length_eval=1000,
+                   replay_buffer=replay_buffer,
+                   min_buffer_size=1e4,
+                   target_update_tau=5e-3,
+                   discount=discount,
+                   buffer_batch_size=optimization_minibatch_size,
+                   reward_scale=1.,
+                   steps_per_epoch=steps_per_epoch,
+                   policy_lr=optimization_lr,
+                   qf_lr=optimization_lr)
+
+    elif algo_name=='TD3':
+        from garage.np.exploration_policies import AddGaussianNoise
+        from garage.np.policies import UniformRandomPolicy
+        from garage.torch.algos import TD3
+        num_timesteps = n_epochs * steps_per_epoch * batch_size
+        policy = get_mlp_policy(stochastic=False, use_tanh=True)
+        exploration_policy = AddGaussianNoise(env.spec,
+                                              policy,
+                                              total_timesteps=num_timesteps,
+                                              max_sigma=0.1,
+                                              min_sigma=0.1)
+        uniform_random_policy = UniformRandomPolicy(env.spec)
+        qf1 = get_mlp_value('Q')
+        qf2 = get_mlp_value('Q')
+        sampler = get_sampler(exploration_policy)
+        replay_buffer = get_replay_buferr()
+        algo = TD3(env_spec=env.spec,
+                  policy=policy,
+                  qf1=qf1,
+                  qf2=qf2,
+                  replay_buffer=replay_buffer,
+                  sampler=sampler,
+                  policy_optimizer=torch.optim.Adam,
+                  qf_optimizer=torch.optim.Adam,
+                  exploration_policy=exploration_policy,
+                  uniform_random_policy=uniform_random_policy,
+                  target_update_tau=0.005,
+                  discount=discount,
+                  policy_noise_clip=0.5,
+                  policy_noise=0.2,
+                  policy_lr=optimization_lr,
+                  qf_lr=optimization_lr,
+                  steps_per_epoch=steps_per_epoch,
+                  start_steps=1000,
+                  grad_steps_per_env_step=optimization_n_grad_steps,  # number of optimization steps
+                  min_buffer_size=int(1e4),
+                  buffer_batch_size=optimization_minibatch_size)
+
+    else:
+        raise ValueError('Unknown algo_name')
+
+
+    if use_gpu:
+        prefer_gpu()
+        if callable(getattr(self, 'to', None)):
+            algo.to()
+
+    return algo
---
+++ shortrl/train_agent.py
@@ -0,0 +1,110 @@
+import torch
+import gym
+
+from garage import wrap_experiment
+from garage.envs import GymEnv
+from garage.experiment.deterministic import set_seed
+from garage.trainer import Trainer
+
+from shortrl.env_wrapper import ShortMDP
+from shortrl.algorithms import get_algo
+from shortrl.heuristics import get_snapshot_values
+
+
+def train_agent(ctxt=None,
+                env_name='InvertedDoublePendulum-v2', # gym env identifier
+                discount=1.0,  # oirginal discount
+                heuristic=None,  # a python function
+                algo_name='PPO',  # algorithm name
+                algo_kwargs=None,  #  algorithm parameter dict
+                lambd=0.9,  # extra discount
+                seed=1,  # random seed
+                n_epochs=50,  # number of updates
+                batch_size=10000,  # number of samples collected per update
+                ):
+
+    assert discount is not None
+    if heuristic is None:
+        heuristic = lambda x : 0.
+
+    set_seed(seed)
+    # Wrap the gym env into our *gym* wrapper first and then into the standard garage wrapper.
+    env = ShortMDP(gym.make(env_name), heuristic, lambd=lambd, gamma=discount)
+    env = GymEnv(env)
+
+    algo = get_algo(env=env,
+                    algo_name=algo_name,
+                    discount=discount*lambd,  #  algorithm sees a shorter horizon,
+                    n_epochs=n_epochs,
+                    batch_size=batch_size,
+                    **algo_kwargs)
+
+    trainer = Trainer(ctxt)
+    trainer.setup(algo, env, lambd)
+    trainer.train(n_epochs=n_epochs, batch_size=batch_size)
+
+
+# Run this.
+def run_exp(exp_name=None,
+            snapshot_frequency=0,
+            log_prefix='agents',
+            **kwargs):
+    snapshot_gap = snapshot_frequency
+    snapshot_mode = 'gap_and_last' if snapshot_frequency>0 else 'last'
+    wrapped_train_agent = wrap_experiment(train_agent,
+                            prefix='experiment/shortrl/'+log_prefix,
+                            snapshot_mode=snapshot_mode,
+                            snapshot_gap=snapshot_gap,
+                            name=exp_name,
+                            use_existing_dir=True)  # overwrites existing directory
+    return wrapped_train_agent(**kwargs)
+
+
+if __name__ == '__main__':
+    # Parse command line inputs.
+    import argparse
+    from shortrl.utils import str2bool
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--lambd', type=float, default=0.9)
+    parser.add_argument('-d', '--discount', type=float, default=1.0)
+    parser.add_argument('-s', '--seed', type=int, default=1)
+    parser.add_argument('-a', '--algo_name', type=str, default='PPO')
+    parser.add_argument('-e', '--env_name', type=str, default='InvertedDoublePendulum-v2')
+    parser.add_argument('-n', '--n_workers', type=int, default=4)
+    parser.add_argument('-u', '--use_heuristic', type=str2bool, default=True)
+    parser.add_argument('-f', '--snapshot_frequency', type=int, default=0)
+    parser.add_argument('-N', '--n_epochs', type=int, default=50)
+    parser.add_argument('-b', '--batch_size', type=int, default=10000)
+    parser.add_argument('--log_prefix', type=str, default='agents')
+    args = parser.parse_args()
+
+    lambd = args.lambd
+    discount = args.discount
+    seed = args.seed
+    env_name=args.env_name
+    algo_name=args.algo_name
+    algo_kwargs={'n_workers':args.n_workers}
+    use_heuristic = args.use_heuristic
+    heuristic = get_snapshot_values(
+                'data/local/experiment/shortrl/heuristics/ppo_pendulum/',
+                itr=19) if use_heuristic else None
+    snapshot_frequency = args.snapshot_frequency
+    n_epochs = args.n_epochs
+    batch_size = args.batch_size
+    log_prefix = args.log_prefix
+
+    # Run experiment.
+    exp_name = algo_name+'_'+env_name[:min(len(env_name),5)]+'_{}_{}_{}'.format(lambd, use_heuristic, seed)
+    run_exp(exp_name=exp_name,
+            env_name=env_name,
+            heuristic=heuristic,
+            discount=discount,
+            algo_name=algo_name,
+            algo_kwargs=algo_kwargs,
+            seed=seed,
+            lambd=lambd,
+            n_epochs=n_epochs,
+            batch_size=batch_size,
+            snapshot_frequency=snapshot_frequency,
+            log_prefix=log_prefix,
+            )
