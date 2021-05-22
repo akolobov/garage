ffe2115bbb38d9a4c7590a759da9ea04d9296bf9
Andrey Kolobov <akolobov@microsoft.com>
2021-05-21 23:21:27 -0700

    setup for running garage on Azure Batch without a mujoco docker container

--- install_srl.sh
+++ install_srl.sh
@@ -1,33 +1,40 @@
-conda create -n gym pip python=3.7 -y
-conda activate gym
+#conda create -n gym pip python=3.7 -y
+#conda activate gym
 
 
 # gym
 sudo apt-get install -y gcc libglu1-mesa-dev libgl1-mesa-dev libosmesa6-dev xvfb ffmpeg curl patchelf libglfw3 libglfw3-dev cmake zlib1g zlib1g-dev swig
 # mkdir repos
 # cd ~/repos
-git clone https://github.com/openai/gym.git
-cd gym
-pip install -e .
+#git clone https://github.com/openai/gym.git
+#cd gym
+#pip install -e .
 
 
 # Mujoco
-cd ~
+#cd ~
 wget https://www.roboti.us/download/mujoco200_linux.zip  --no-check-certificate
 # sudo apt install unzip
 mkdir ~/.mujoco
+#mkdir ~/opt/.mujoco
+#unzip mujoco200_linux.zip -d /opt/.mujoco
 unzip mujoco200_linux.zip -d ~/.mujoco
 rm mujoco200_linux.zip
 mv ~/.mujoco/mujoco200_linux ~/.mujoco/mujoco200
-cp ~/mjkey.txt ~/.mujoco
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.mujoco/mujoco200/bin" >> ~/.bashrc
+#mv /opt/.mujoco/mujoco200_linux /opt/mujoco200_linux
+cp ../mjkey.txt ~/.mujoco
+#cp mjkey.txt /opt
+#echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.mujoco/mujoco200/bin" >> ~/.bashrc
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.mujoco/mujoco200/bin
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/mujoco200_linux/bin
+#export MUJOCO_PY_MJKEY_PATH=/opt/mjkey.txt
+#export MUJOCO_PY_MUJOCO_PATH=/opt/mujoco200_linux
 pip install -U 'mujoco-py<2.1,>=2.0'
 # python -c 'import mujoco_py'
 
 # garage
-cd ~
-git clone https://github.com/akolobov/garage.git
-cd garage
-git checkout shortrl
-pip install -e '.[all,dev]'
+#cd ~
+#git clone https://github.com/akolobov/garage.git
+#cd garage
+#git checkout shortrl
+#pip install -e '.[all,dev]'
